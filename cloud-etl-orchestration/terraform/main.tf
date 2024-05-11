fd3c26b32ae20930f2c9da5694bd5484f821389b
Luis Olcay <luis.olcay@uc.cl>
2024-05-11 12:03:33 -0400

    Aligned main.tf for the beam pipeline: add tables and schemas

--- cloud-etl-orchestration/terraform/main.tf
+++ cloud-etl-orchestration/terraform/main.tf
@@ -37,15 +37,163 @@ resource "google_storage_bucket" "gcs_chilean_football" {
   }
 }
 
-#Bigquery
+# BigQuery Dataset
 resource "google_bigquery_dataset" "dw_bigquery" {
-  dataset_id = "stadium_data"
-  location = "southamerica-east1"
+  dataset_id = "chilean_premier_league_2024"
+  location   = "southamerica-east1"
 }
 
-resource "google_bigquery_table" "sa_stadium_table" {
+# BigQuery Table for Teams
+resource "google_bigquery_table" "teams_2024" {
   dataset_id = google_bigquery_dataset.dw_bigquery.dataset_id
-  table_id   = "southamerica-stadiums" 
+  table_id   = "teams_2024"
   deletion_protection = false
 
+  schema = jsonencode([
+    {
+      name = "team_id",
+      type = "INTEGER",
+      mode = "REQUIRED"
+    },
+    {
+      name = "country",
+      type = "STRING",
+      mode = "NULLABLE"
+    },
+    {
+      name = "venue_city",
+      type = "STRING",
+      mode = "NULLABLE"
+    },
+    {
+      name = "name",
+      type = "STRING",
+      mode = "NULLABLE"
+    },
+    {
+      name = "founded",
+      type = "INTEGER",
+      mode = "NULLABLE"
+    },
+    {
+      name = "venue_name",
+      type = "STRING",
+      mode = "NULLABLE"
+    }
+  ])
+}
+
+# BigQuery Table for Scorers Information
+resource "google_bigquery_table" "scorers_information_2024" {
+  dataset_id = google_bigquery_dataset.dw_bigquery.dataset_id
+  table_id   = "scorers_information_2024"
+  deletion_protection = false
+
+  schema = jsonencode([
+    {
+      name = "firstname",
+      type = "STRING",
+      mode = "NULLABLE"
+    },
+    {
+      name = "lastname",
+      type = "STRING",
+      mode = "NULLABLE"
+    },
+    {
+      name = "age",
+      type = "INTEGER",
+      mode = "NULLABLE"
+    },
+    {
+      name = "nationality",
+      type = "STRING",
+      mode = "NULLABLE"
+    },
+    {
+      name = "height",
+      type = "STRING",
+      mode = "NULLABLE"
+    },
+    {
+      name = "weight",
+      type = "STRING",
+      mode = "NULLABLE"
+    },
+    {
+      name = "injured",
+      type = "BOOLEAN",
+      mode = "NULLABLE"
+    },
+    {
+      name = "team",
+      type = "STRING",
+      mode = "NULLABLE"
+    }
+  ])
+}
+
+# BigQuery Table for Scorers Statistics
+resource "google_bigquery_table" "scorers_statistics_2024" {
+  dataset_id = google_bigquery_dataset.dw_bigquery.dataset_id
+  table_id   = "scorers_statistics_2024"
+  deletion_protection = false
+
+  schema = jsonencode([
+    {
+      name = "firstname",
+      type = "STRING",
+      mode = "NULLABLE"
+    },
+    {
+      name = "lastname",
+      type = "STRING",
+      mode = "NULLABLE"
+    },
+    {
+      name = "goals",
+      type = "INTEGER",
+      mode = "NULLABLE"
+    },
+    {
+      name = "assists",
+      type = "INTEGER",
+      mode = "NULLABLE"
+    },
+    {
+      name = "conceded",
+      type = "INTEGER",
+      mode = "NULLABLE"
+    },
+    {
+      name = "penalty_scored",
+      type = "INTEGER",
+      mode = "NULLABLE"
+    },
+    {
+      name = "penalty_missed",
+      type = "INTEGER",
+      mode = "NULLABLE"
+    },
+    {
+      name = "total_passes",
+      type = "INTEGER",
+      mode = "NULLABLE"
+    },
+    {
+      name = "key_passes",
+      type = "INTEGER",
+      mode = "NULLABLE"
+    },
+    {
+      name = "total_duels",
+      type = "INTEGER",
+      mode = "NULLABLE"
+    },
+    {
+      name = "duels_won",
+      type = "INTEGER",
+      mode = "NULLABLE"
+    }
+  ])
 }
